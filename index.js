ca0041e4a0a6d4afc1a781a261affbeb8430a9f4
STHDatabase <52685326+STHDatabase@users.noreply.github.com>
2019-07-11 06:48:32 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,52 @@
+const bot = new roblox.api({userlogin: true});
+
+bot.joinGame=({link: https://www.roblox.com/games/155615604/Prison-Life-Cars-fixed}) --> {
+               bot.followPlayer(userName);
+if userName is true = bot.userName(x,y,z);
+               then if userName loc bot.xyzMovement(follow);
+if  userName = bot.userName(target);
+               then bot.xyzMovement(stop);
+              }
+            }
+         }
+bot.setKey --> {
+function bot presskey up bot.key(w); 
+function bot presskey left bot.key(s);
+function bot presskey right bot.key(a);
+function bot presskey down bot.key(d);
+function bot presskey fight bot.key(f);
+function bot presskey crouch bot.key(c);
+function bot presskey jump bot.key(space);
+function bot mouse bot.mouse(target);
+});               
+
+bot.target(userName);
+if target bot userName.bot(bot) --> {
+          then bot.userName(target);
+if target bot bot.target(mouse);
+          then bot.userName(target);
+         }
+       }
+    }
+if bot.trigger(m8) --> {
+   then bot.target(m8);
+if bot.trigger(remington);
+   then bot.target(remington);
+if bot.trigger(hammer);
+   then bot.target(hammer);
+});  
+
+if bot.process(userName) = true --> {
+    then bot.target(userName);
+});
+
+if bot.target(userName) = true --> {
+    then bot.mouse(target);
+});
+               
+if bot.target(userName) = true --> {
+    then bot.presskey(space);
+}); 
+
+
+bot.login(process.env.TheUItimateDoom);
